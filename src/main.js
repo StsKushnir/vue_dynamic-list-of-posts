b5aee168ca5c970228e534f1e3b008ea87a5c08e
Stanislav <florian.of.lorch@gmail.com>
2024-08-19 18:58:58 +0300

    add base structure

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/styles/App.scss';
+import './assets/styles/Loader.scss';
+
+import 'bulma/bulma.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+createApp(App).mount('#app')
